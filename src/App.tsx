a30169b1997b18cb7e6a993b0721b6c29b9ac38e
mliem2k <michaelbstyle@gmail.com>
2024-02-09 01:26:20 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { About } from "./components/About";
-import { Cta } from "./components/Cta";
+// import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
+// import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
+// import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
 import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
+// import { Team } from "./components/Team";
 import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
@@ -22,12 +22,12 @@ function App() {
       <Hero />
       <Sponsors />
       <About />
-      <HowItWorks />
-      <Features />
+      {/* <HowItWorks /> */}
+      {/* <Features /> */}
       <Services />
-      <Cta />
+      {/* <Cta /> */}
       <Testimonials />
-      <Team />
+      {/* <Team /> */}
       <Pricing />
       <Newsletter />
       <FAQ />
